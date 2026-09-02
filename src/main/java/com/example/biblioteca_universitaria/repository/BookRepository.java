4e49f414536f2105f3f586480c1bd4afe5380490
larissa557197 <larissampmuniz@gmail.com>
2026-09-01 22:45:38 -0300

    feat: cria a interface BookRepository com verificacao de ISBN

--- src/main/java/com/example/biblioteca_universitaria/repository/BookRepository.java
+++ src/main/java/com/example/biblioteca_universitaria/repository/BookRepository.java
@@ -1,10 +1,14 @@
+// Define o pacote onde as interfaces de acesso ao banco de dados estão localizadas
 package com.example.biblioteca_universitaria.repository;
 
+// Importação da entidade Book
 import com.example.biblioteca_universitaria.domain.Book;
+// Importação da interface base do Spring Data JPA
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    // Verifica no banco se já existe algum livro cadastrado com o ISBN informado
     boolean existsByIsbn(String isbn);
 }
