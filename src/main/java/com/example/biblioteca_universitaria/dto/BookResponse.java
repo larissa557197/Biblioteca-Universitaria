5eacbb040556fd68d30fd442d53f318d9a452b70
larissa557197 <larissampmuniz@gmail.com>
2026-09-01 23:08:49 -0300

    feat: adiciona DTOs BookRequest e BookResponse para gestao de livros

--- src/main/java/com/example/biblioteca_universitaria/dto/BookResponse.java
+++ src/main/java/com/example/biblioteca_universitaria/dto/BookResponse.java
@@ -1,6 +1,7 @@
 // Define o pacote onde os Data Transfer Objects (DTOs) estão localizados
 package com.example.biblioteca_universitaria.dto;
 
+import com.example.biblioteca_universitaria.domain.Book;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,13 +21,12 @@ public class BookResponse {
     private Integer exemplaresDisponiveis;
 
     // Construtor utilitário para converter uma entidade Book diretamente em BookResponse
-
-    public BookResponse(Long id, String titulo, String autor, String isbn, Integer totalExemplares, Integer exemplaresDisponiveis) {
-        this.id = id;
-        this.titulo = titulo;
-        this.autor = autor;
-        this.isbn = isbn;
-        this.totalExemplares = totalExemplares;
-        this.exemplaresDisponiveis = exemplaresDisponiveis;
+    public BookResponse(Book book){
+        this.id = book.getId();
+        this.titulo = book.getTitulo();
+        this.autor = book.getAutor();
+        this.isbn = book.getIsbn();
+        this.totalExemplares = book.getTotalExemplares();
+        this.exemplaresDisponiveis = book.getExemplaresDisponiveis();
     }
 }
